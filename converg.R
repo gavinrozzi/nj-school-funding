bd52bfc321b4973d7e7350e69ce20cc40f8bd6a2
Gavin Rozzi <gr@gavinrozzi.com>
2021-02-07 14:10:21 -0500

    Start working on R translation

---
+++ converg.R
@@ -0,0 +1,21 @@
+##############################################################
+# R implementation of the New Jersey School Funding Formula  #
+# Gavin Rozzi                                                #
+##############################################################
+
+# Initialize flag at 0
+flag <- 0
+
+# Iterate until convergence is reached
+while (flag = 0)
+{
+  #do something
+}
+
+if (cyc = 1) {
+  
+} else if (cyc = 2) {
+  
+} else if (cyc = 2) {
+  
+}
